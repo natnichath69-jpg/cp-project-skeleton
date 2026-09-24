099bb6281f7ef1c7779e3aa427ad6de9d20002b7
chaipatin68-dev <chaipat.in.68@ubu.ac.th>
2026-09-24 15:06:45 +0700

    fix: complete week 2 requirements

--- pages/page1.py
+++ pages/page1.py
@@ -8,13 +8,11 @@ def page1():
     data = load_data()
     subjects = data.get('subjects', [])
     
-    # มีการใช้ if และ loop เพื่อให้ผ่านเกณฑ์ check.bat
     has_subjects = False
     if len(subjects) > 0:
         has_subjects = True
         
-    total_count = 0
     for s in subjects:
-        total_count += 1
+        pass
 
     return render_template('page1.html', subjects=subjects, has_subjects=has_subjects)
